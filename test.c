957b2bbcf9b9ab5d943a2c33b464ac048068332a
Suman <suman@ubuntu.(none)>
2013-06-05 15:52:13 -0400

    realloc, free

--- test.c
+++ test.c
@@ -7,6 +7,8 @@
 #define	RSLOP		0
 #endif
 
+static int findbucket();
+
 #define	NBUCKETS 30
 union overhead *nextf[NBUCKETS];
 
@@ -33,6 +35,27 @@ union	overhead {
 #define	ov_rmagic	ovu.ovu_rmagic
 #define	ov_size		ovu.ovu_size
 };
+
+int realloc_srchlen = 4;
+
+static
+findbucket(freep, srchlen)
+	union overhead *freep;
+	int srchlen;
+{
+	register union overhead *p;
+	register int i, j;
+
+	for (i = 0; i < NBUCKETS; i++) {
+		j = 0;
+		for (p = nextf[i]; p && j != srchlen; p = p->ov_next) {
+			if (p == freep)
+				return (i);
+			j++;
+		}
+	}
+	return (-1);
+}
 
 void *
 myalloc(nbytes)
@@ -171,7 +194,105 @@ myalloc(nbytes)
   	return ((char *)(op + 1));
 }
 
+void *
+myrealloc(cp, nbytes)
+	void *cp; 
+	unsigned nbytes;
+{   
+  	register uint8_t onb;
+	register int i;
+	union overhead *op;
+  	char *res;
+	int was_alloced = 0;
 
+  	if (cp == NULL)
+  		return (malloc(nbytes));
+
+	op = (union overhead *)((caddr_t)cp - sizeof (union overhead));
+	if (op->ov_magic == MAGIC) {
+		was_alloced++;
+		i = op->ov_index;
+	} else {
+		/*
+		 * Already free, doing "compaction".
+		 *
+		 * Search for the old block of memory on the
+		 * free list.  First, check the most common
+		 * case (last element free'd), then (this failing)
+		 * the last ``realloc_srchlen'' items free'd.
+		 * If all lookups fail, then assume the size of
+		 * the memory block being realloc'd is the
+		 * largest possible (so that all "nbytes" of new
+		 * memory are copied into).  Note that this could cause
+		 * a memory fault if the old area was tiny, and the moon
+		 * is gibbous.  However, that is very unlikely.
+		 */
+		if ((i = findbucket(op, 1)) < 0 &&
+		    (i = findbucket(op, realloc_srchlen)) < 0)
+			i = NBUCKETS;
+	}
+	onb = 1 << (i + 3);
+	if (onb < pagesz)
+		onb -= sizeof (*op) + RSLOP;
+	else
+		onb += pagesz - sizeof (*op) - RSLOP;
+	/* avoid the copy if same size block */
+	if (was_alloced) {
+		if (i) {
+			i = 1 << (i + 2);
+			if (i < pagesz)
+				i -= sizeof (*op) + RSLOP;
+			else
+				i += pagesz - sizeof (*op) - RSLOP;
+		}
+		if (nbytes <= onb && nbytes > i) {
+#ifdef RCHECK
+			op->ov_size = (nbytes + RSLOP - 1) & ~(RSLOP - 1);
+			*(u_short *)((caddr_t)(op + 1) + op->ov_size) = RMAGIC;
+#endif
+
+			return(cp);
+		} else
+			free(cp);
+	}
+  	if ((res = malloc(nbytes)) == NULL)
+	{
+
+  		return (NULL);
+	}
+  	if (cp != res)		/* common optimization if "compacting" */
+		memcpy(res, cp, (nbytes < onb) ? nbytes : onb);
+
+  	return (res);
+}
+
+myfree(cp)
+	void *cp;
+{   
+  	register int size;
+	register union overhead *op;
+
+  	if (cp == NULL)
+  		return;
+
+	op = (union overhead *)((caddr_t)cp - sizeof (union overhead));
+#ifdef DEBUG
+#else
+	if (op->ov_magic != MAGIC)
+	{
+
+		return;				
+	}
+#endif
+#ifdef RCHECK
+#endif
+  	size = op->ov_index;
+	op->ov_next = nextf[size];	
+  	nextf[size] = op;
+#ifdef MSTATS
+  	nmalloc[size]--;
+#endif
+}
 
 typedef struct point{
 int x,y;
@@ -183,8 +304,16 @@ void main(){
 	b.y = 20;
 	p * a; 
 	a = (p*)myalloc(sizeof(struct point));
+	a = &b;
 	a->x = 30;
 	a->y = 40;
+	printf("%ld\n",a);
+	printf("%d\n%d\n",a->x,a->y);
+	a = (p*)myrealloc(a, 2*sizeof(struct point));
+//	a->x = 50;
+//	a->y = 60;
 	printf("%ld\n%d\n%d\n%d\n%d\n",a,b.x,b.y,a->x,a->y);
 	printf("hello world\n");
+	myfree(a);
+	printf("%ld\n%d\n",a,a->x);
 }
